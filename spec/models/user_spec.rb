0e8ff32fab96afae7eee234e6c97c79370087e74
yupi-pi <lensyonen@gmail.com>
2023-03-15 15:28:50 +0900

    デバッグ用の記述を削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -108,7 +108,6 @@ RSpec.describe User, type: :model do
       it '生年月日が空欄だと保存できない' do
         @user.birthday = ''
         @user.valid?
-        # binding.pry
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
     end
