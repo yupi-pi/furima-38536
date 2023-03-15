d9b869c75a80ec340451ab8145f9a99d9f40d76d
yupi-pi <lensyonen@gmail.com>
2023-03-15 15:25:07 +0900

    今後の実装要件のためコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 
   with_options presence: true do
     validates :nickname
