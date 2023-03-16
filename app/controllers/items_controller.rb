d9879af13f4e391712699f2283182f0bd0abe1cd
yupi-pi <lensyonen@gmail.com>
2023-03-16 15:22:40 +0900

    itemコントローラーにnewアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
+  end
+
+  def new
   end
 end
