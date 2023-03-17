0cfd24abf76267450533b8b68e0d48e736417607
yupi-pi <lensyonen@gmail.com>
2023-03-17 17:12:58 +0900

    itemモデルテストコードの実装
    テスト用の画像も実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :delivery_cost_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :delivery_cost_id, :prefecture_id,
+                                 :delivery_date_id, :price).merge(user_id: current_user.id)
   end
-
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,101 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.create(:item)
+  end
+
+  describe '商品の出品登録' do
+    context '出品登録ができるとき' do
+      it '全ての入力事項が、存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+      it 'カテゴリーが「---」以外であれば登録できる' do
+        @item.category_id = 1
+        expect(@item).to be_valid
+      end
+      it '商品の状態が「---」以外であれば登録できる' do
+        @item.item_status_id = 1
+        expect(@item).to be_valid
+      end
+      it '配送料の負担が「---」以外であれば登録できる' do
+        @item.delivery_cost_id = 1
+        expect(@item).to be_valid
+      end
+      it '発送元の地域が「---」以外であれば登録できる' do
+        @item.prefecture_id = 1
+        expect(@item).to be_valid
+      end
+      it '発送までの日数が「---」以外であれば登録できる' do
+        @item.delivery_date_id = 1
+        expect(@item).to be_valid
+      end
+      it '価格が半角数字でかつ300円〜9,999,999円であれば登録できる' do
+        @item.price = 300
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品ができないとき' do
+      it '１枚画像がないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空欄だと出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明が空欄だと出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'カテゴリーの情報が「---」だと出品できない' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態の情報が「---」だと出品できない' do
+        @item.item_status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item status can't be blank")
+      end
+      it '配送料の負担の情報が「---」だと出品できない' do
+        @item.delivery_cost_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery cost can't be blank")
+      end
+      it '発送元の地域の情報が「---」だと出品できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数の情報が「---」だと出品できない' do
+        @item.delivery_date_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery date can't be blank")
+      end
+      it '価格が空欄だと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
+      end
+      it '価格が全角だと出品できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '価格の範囲が、300円〜9,999,999円以外だと出品できない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it '価格の範囲が、9,999,999円を超えると出品できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+    end
+  end
 end
