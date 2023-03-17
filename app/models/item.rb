7e27e063c178b51b530e779bcfa513d0d4ae937f
yupi-pi <lensyonen@gmail.com>
2023-03-17 20:29:07 +0900

    numericalityのオプションを追加し、不要な記述を削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,8 +22,7 @@ class Item < ApplicationRecord
     validates :delivery_cost_id
     validates :prefecture_id
     validates :delivery_date_id
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   with_options numericality: { other_than: 0, message: "can't be blank" } do
