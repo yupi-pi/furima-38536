99e765aa7181322f691612b4df24034016d4d14e
yupi-pi <lensyonen@gmail.com>
2023-03-20 21:14:27 +0900

    itemコントローラーにeditアクションを定義
    編集画面へ遷移するパスを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   def item_params
