443d2ecd6db6e1191b9362fa39420ac75f9f3a55
yupi-pi <lensyonen@gmail.com>
2023-03-21 14:27:51 +0900

    コードをよりシンプルにするために修正
    ==を!=に変えると、else無しで実装可能だから

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id
-    else
-      redirect_to root_path
+    if @item.user_id != current_user.id
     end
   end
 
