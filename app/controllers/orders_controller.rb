1d29221ca21f2835e641df3ee0c4f81884efcb59
yupi-pi <lensyonen@gmail.com>
2023-03-23 01:55:24 +0900

    URLを直接入力して、購入画面へ遷移しないように実装
    URLを入力して売り切れ商品の場合はトップページへ遷移。出品者は購入できないようにトップページへ遷移。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,13 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :non_soldout_item, only: [:index, :create]
 
   def index
     @order_form = OrderForm.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @order_form = OrderForm.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_form.valid?
       @order_form.save
       redirect_to root_path
@@ -21,5 +20,10 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def non_soldout_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
