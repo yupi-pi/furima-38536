084f61e0b20d98013538c6d72e64ae0718584f97
yupi-pi <lensyonen@gmail.com>
2023-03-22 15:56:50 +0900

    itemモデルを修正
    新規テーブルの作成に伴い、アソシエーションを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   # テーブルとのアソシエーション
   belongs_to :user
-  # has_one :order
+  has_one :order
 
   # active_storageとのアソシエーション
   has_one_attached :image
