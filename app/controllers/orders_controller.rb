07aab98004b78c4e1c71962db164f22880f33cea
yupi-pi <lensyonen@gmail.com>
2023-03-24 16:38:01 +0900

    rubocopで修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,12 +5,10 @@ class OrdersController < ApplicationController
   def index
     if current_user.id == @item.user_id
       redirect_to root_path
+    elsif @item.order.nil?
+      @order_form = OrderForm.new
     else
-      if @item.order == nil
-        @order_form = OrderForm.new
-      else
-        redirect_to root_path
-      end
+      redirect_to root_path
     end
   end
 
@@ -28,19 +26,21 @@ class OrdersController < ApplicationController
   private
 
   def order_form_params
-    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
-  
+
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_form_params[:token],
-        currency: 'jpy'
-      )
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_form_params[:token],
+      currency: 'jpy'
+    )
   end
 end
